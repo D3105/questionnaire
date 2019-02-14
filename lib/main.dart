ca8b73425d9724d4a0dc4934836745be3c5d6843
d <rlh1191@gmail.com>
2019-02-14 08:40:17 +0200

    extract App class

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:questionnaire/src/app.dart';
 import 'package:questionnaire/src/screens/questionnaire_list_screen.dart';
 
 void main() => runApp(App());
-
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Questionnaire',
-      home: QuestionnaireListScreen(),
-    );
-  }
-}
