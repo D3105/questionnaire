54fa10ecd8cd88b1a9b6c6879e0e829bbfe94eec
d <rlh1191@gmail.com>
2019-03-13 22:31:33 +0200

    refactoring quiz result

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:questionnaire/src/app.dart';
-import 'package:questionnaire/src/widgets/overall_quiz_result.dart';
+import 'package:questionnaire/src/screens/quiz_result_screen.dart';
 
 Future<void> main() async {
   await Firestore.instance.settings(timestampsInSnapshotsEnabled: true);
   runApp(App());
-  //runApp(MaterialApp(home: OverallQuizResult()));
+  // runApp(MaterialApp(home: QuizResultScreen()));
 }
